9f04948d4ebb44a27bcb6d0a3e8b08969acf3b3b
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-04-19 17:04:51 +0900

    feat: 예외 처리를 위한 ControllerAdvice추가

---
+++ src/main/java/com/spring/shop/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.spring.shop.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(GlobalException.class)
+	protected ResponseEntity<String> handleTesseractException(GlobalException e){
+		
+		e.printStackTrace();
+		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
