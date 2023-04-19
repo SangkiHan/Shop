b7528df4188a8802f8b369eaef2286e5bbd90efd
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-04-19 17:00:52 +0900

    feat: 공통 Exception개발

---
+++ src/main/java/com/spring/shop/Exception/GlobalException.java
@@ -0,0 +1,26 @@
+package com.spring.shop.Exception;
+
+public class GlobalException extends RuntimeException  {
+	
+	private static final long serialVersionUID = 8636904039967520807L;
+	
+	private String message;
+	
+	public GlobalException(String message, Exception e) {
+		super(message, e);
+		this.message = message;
+	}
+	
+	public GlobalException(String message) {
+		super(message);
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		if(message == null || message.length() == 0) {
+				return "요청처리에 실패하였습니다.";
+		}
+		
+		return message;
+	}
+}
