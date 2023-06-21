9782cd57b84e9e3d94b87a9ae938f37d41aa5712
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-06-21 12:55:23 +0900

    fix: 카테고리 삭제

--- src/main/java/com/spring/shop/controller/dto/OrderItemDto.java
+++ src/main/java/com/spring/shop/controller/dto/OrderItemDto.java
@@ -1,20 +1,19 @@
 package com.spring.shop.controller.dto;
 
 import com.spring.shop.entity.OrderItem;
-import com.spring.shop.entity.item.Item;
 
 import lombok.Data;
 
 @Data
 public class OrderItemDto {
 	private Long Id;
-	private Item item;
+	private ItemDto.Info item;
 	private int orderPrice;
 	private int count;
 	
 	public OrderItemDto(OrderItem orderItem) {
 		Id = orderItem.getId();
-		this.item = orderItem.getItem();
+		this.item =  new ItemDto.Info(orderItem.getItem());
 		this.orderPrice = orderItem.getOrderPrice();
 		this.count = orderItem.getCount();
 	}
